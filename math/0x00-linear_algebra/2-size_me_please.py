2ba43b9e49f7d074b6756dcf58f20c47ed3563bd
oran2527 <orlago250183@gmail.com>
2021-01-19 17:05:43 +0000

    program to define the size of a matrix

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" matrix shape calculator """ 
+
+
+def matrix_shape(matrix):
+    """ matrix to define a matrix """
+    
+    size = []
+    try:   
+        size.append(len(matrix))
+        size.append(len(matrix[0]))
+        size.append(len(matrix[0][0]))
+        return size
+    except:
+        return size             	
