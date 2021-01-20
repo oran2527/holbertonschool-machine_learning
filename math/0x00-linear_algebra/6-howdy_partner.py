5ab876e7cb1761c35ff0a858f4a7bad9aebae256
oran2527 <orlago250183@gmail.com>
2021-01-20 15:48:09 +0000

    program to concatenate two arrays

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" program to concatenate two arrays """
+
+
+def cat_arrays(arr1, arr2):
+    """ function to concatenate """
+
+    finalarr = []
+    for i in arr1:
+        finalarr.append(i)
+    for i in arr2:
+        finalarr.append(i)         
+    return finalarr 
