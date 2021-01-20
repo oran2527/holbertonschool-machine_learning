c3411924f5f28badc261e2f4be8eaff907966334
oran2527 <orlago250183@gmail.com>
2021-01-20 23:43:09 +0000

    program to calculate transpose of a matrix

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" program to calculate the transpose of a matrix """
+
+
+def np_transpose(matrix):
+    """ function to return the transpose of a matrix """
+
+    import numpy as np
+
+    return np.transpose(matrix)
