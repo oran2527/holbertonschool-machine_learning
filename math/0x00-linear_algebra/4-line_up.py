ee890471bf1bb031ad17f42f1c5803de759c9999
oran2527 <orlago250183@gmail.com>
2021-01-20 15:25:13 +0000

    fixing spaces

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -3,32 +3,32 @@
 
 def add_arrays(arr1, arr2):
     """ function to add two arrays """
-    
+
     cursize1 = []
     cursize2 = []
     finallist = []
     flag = 0
 
-    cursize1 = matrix_shape(arr1)                  
+    cursize1 = matrix_shape(arr1)
     cursize2 = matrix_shape(arr2)
-     
+
     if len(cursize1) == len(cursize2):
         for i in range(0, len(cursize1)):
             if cursize1[i] != cursize2[i]:
                 flag = 1
                 break
-        if flag != 1:            
-            for i in range(0, len(arr1)):                  
-                finallist.append(arr1[i] + arr2[i])                 
+        if flag != 1:
+            for i in range(0, len(arr1)):
+                finallist.append(arr1[i] + arr2[i])
             return finallist
     else:
-        return None        
+        return None
 
 def matrix_shape(matrix_sub):
     """ matrix to define a matrix """
-    
+
     size = []
-    try:   
+    try:
         size.append(len(matrix_sub))
         size.append(len(matrix_sub[0]))
         size.append(len(matrix_sub[0][0]))
