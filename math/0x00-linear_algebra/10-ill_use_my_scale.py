62b6d3c0a42beeae80bc35bc33fdce110ea58b90
oran2527 <orlago250183@gmail.com>
2021-01-20 23:31:48 +0000

    program to get the shape of a matrix

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" program to calculate the shape of a matrix """
+
+
+def np_shape(matrix):
+    """ function to return the shape of a matrix """
+
+    import numpy as np
+
+    return matrix.shape
