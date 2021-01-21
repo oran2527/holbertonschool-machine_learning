c2ae9202a2935837cf82a3a1edbb937a6100db54
oran2527 <orlago250183@gmail.com>
2021-01-21 00:14:02 +0000

    program to multiply two matrices

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" program to multiply two matrices """
+
+
+def np_matmul(mat1, mat2):
+    """ function to return the mult of two matrices """
+
+    import numpy as np
+
+    return np.matmul(mat1, mat2)
