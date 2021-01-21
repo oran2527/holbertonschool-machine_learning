4ac84e489e056cf4430269ed0fa697f70c97dfc4
oran2527 <orlago250183@gmail.com>
2021-01-21 18:37:35 +0000

    putting conditional for type

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -9,8 +9,10 @@ def matrix_shape(matrix):
     try:
         if type(matrix) == list: 
             size.append(int(len(matrix)))
+        if type(matrix[0]) == list:  
             size.append(int(len(matrix[0])))
+        if type(matrix[0][0]) == list:
             size.append(int(len(matrix[0][0])))
-            return size
+        return size
     except Exception as ex:
         return size
