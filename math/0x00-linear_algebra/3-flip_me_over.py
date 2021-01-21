5cbff7eb732390a1a0456691fa7ba395a85e3da1
oran2527 <orlago250183@gmail.com>
2021-01-21 16:54:38 +0000

    fixing pycodestyle errors

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -1,40 +1,39 @@
 #!/usr/bin/env python3
 """ matrix transposer """
 
+
 def matrix_transpose(matrix):
     """ function to transpose a matrix """
-    
+
     newmat = []
     newmat2 = []
     finalmat = []
     cursize = []
 
-    cursize = matrix_shape(matrix)                  
+    cursize = matrix_shape(matrix)
     for i in range(0, cursize[0]):
         for j in range(0, cursize[1]):
-            newmat.append(matrix[i][j])                     
-    for i in range(0, cursize[1]):        
+            newmat.append(matrix[i][j])
+    for i in range(0, cursize[1]):
         newmat2.append(newmat[i])
-        j = i;
-        while(len(newmat2) < cursize[0]):              
-            if j < len(newmat):  
+        j = i
+        while(len(newmat2) < cursize[0]):
+            if j < len(newmat):
                 newmat2.append(newmat[j + cursize[1]])
-                j = j + cursize[1] 
+                j = j + cursize[1]
         finalmat.append(newmat2)
-        newmat2 = []  
-    return finalmat    
-    
+        newmat2 = []
+    return finalmat
 
 
 def matrix_shape(matrix_sub):
     """ matrix to define a matrix """
-    
+
     size = []
-    try:   
+    try:
         size.append(len(matrix_sub))
         size.append(len(matrix_sub[0]))
         size.append(len(matrix_sub[0][0]))
         return size
-    except:
+    except Exception as ex:
         return size
-                     	    
