da5c7f21c66378f644993f37629d1375ef64264c
oran2527 <orlago250183@gmail.com>
2021-01-21 17:24:06 +0000

    fixing trailing whitespace line 7

--- math/0x00-linear_algebra/14-saddle_up.py
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -4,6 +4,6 @@ import numpy as np
 
 
 def np_matmul(mat1, mat2):
-    """ function to return the mult of two matrices """  
+    """ function to return the mult of two matrices """
 
     return np.matmul(mat1, mat2)
