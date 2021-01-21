42e2d77eb5478c3a6007a9972683075e94898c99
oran2527 <orlago250183@gmail.com>
2021-01-21 00:16:16 +0000

    changing position import

--- math/0x00-linear_algebra/14-saddle_up.py
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """ program to multiply two matrices """
+import numpy as np
 
 
 def np_matmul(mat1, mat2):
-    """ function to return the mult of two matrices """
-
-    import numpy as np
+    """ function to return the mult of two matrices """  
 
     return np.matmul(mat1, mat2)
