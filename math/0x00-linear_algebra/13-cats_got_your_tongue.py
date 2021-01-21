d3ffe2bd12379d813ff16eaf932dada1436c3ec9
oran2527 <orlago250183@gmail.com>
2021-01-21 00:17:39 +0000

    changing position import

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """ program to concatenate two matrices along specific axis """
+import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
     """ function to return a new numpy.ndarray from two matrices along a specific axis """
-
-    import numpy as np
 
     return np.concatenate((mat1, mat2), axis)
