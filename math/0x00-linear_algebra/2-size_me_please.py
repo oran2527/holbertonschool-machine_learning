cc50c31522b0624184488807e08699ab1de87f3f
oran2527 <orlago250183@gmail.com>
2021-01-21 17:26:31 +0000

    fixing pycodestyle errors

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-""" matrix shape calculator """ 
+""" matrix shape calculator """
 
 
 def matrix_shape(matrix):
     """ matrix to define a matrix """
-    
+
     size = []
-    try:   
+    try:
         size.append(len(matrix))
         size.append(len(matrix[0]))
         size.append(len(matrix[0][0]))
         return size
-    except:
-        return size             	
+    except Exception as ex:
+        return size
