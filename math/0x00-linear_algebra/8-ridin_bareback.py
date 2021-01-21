e209585e9b14876288d4795d89dc313e2826b4b3
oran2527 <orlago250183@gmail.com>
2021-01-21 17:09:00 +0000

    fixing pycodestyle errors

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ matrix multiplication """
 
+
 def mat_mul(mat1, mat2):
     """ function to multiply two matrices """
 
@@ -9,7 +10,7 @@ def mat_mul(mat1, mat2):
     newmat = []
     add = 0
     finalmat = []
-    
+
     cursize1 = matrix_shape(mat1)
     cursize2 = matrix_shape(mat2)
 
@@ -17,7 +18,7 @@ def mat_mul(mat1, mat2):
         for i in range(0, len(mat1)):
             for w in range(0, len(mat2[0])):
                 for m in range(0, len(mat2)):
-                    add = add + mat1[i][m] * mat2[m][w] 
+                    add = add + mat1[i][m] * mat2[m][w]
                 newmat.append(add)
                 add = 0
             finalmat.append(newmat)
@@ -31,10 +32,10 @@ def matrix_shape(matrix_sub):
     """ matrix to define a matrix """
 
     size = []
-    try:   
+    try:
         size.append(len(matrix_sub))
         size.append(len(matrix_sub[0]))
         size.append(len(matrix_sub[0][0]))
         return size
-    except:
+    except Exception as ex:
         return size
