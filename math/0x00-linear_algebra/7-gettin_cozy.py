4ce005d592428212baa2175bcd0c28f9c836ab81
oran2527 <orlago250183@gmail.com>
2021-01-21 18:28:59 +0000

    fixing pycodestyle errors

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ matrix 2D concatenater """
 
+
 def cat_matrices2D(mat1, mat2, axis=0):
     """ function to concatenate two 2d matrices """
 
@@ -8,11 +9,10 @@ def cat_matrices2D(mat1, mat2, axis=0):
     cursize2 = []
     newmat = []
     finallist = []
-       
-    
+
     cursize1 = matrix_shape(mat1)
-    cursize2 = matrix_shape(mat2)    
-        
+    cursize2 = matrix_shape(mat2)
+
     if axis == 0:
         for i in mat1:
             finallist.append(i.copy())
@@ -22,23 +22,23 @@ def cat_matrices2D(mat1, mat2, axis=0):
     else:
         if axis > 0:
             for i in mat1:
-                newmat.append(i.copy())            
+                newmat.append(i.copy())
             for i in range(0, len(newmat)):
                 for j in range(0, len(mat2[i])):
-                    newmat[i].append(mat2[i][j])             
-            return newmat                            
+                    newmat[i].append(mat2[i][j])
+            return newmat
         else:
-            return None  
+            return None
 
 
 def matrix_shape(matrix_sub):
     """ matrix to define a matrix """
 
     size = []
-    try:   
+    try:
         size.append(len(matrix_sub))
         size.append(len(matrix_sub[0]))
         size.append(len(matrix_sub[0][0]))
         return size
-    except:
+    except Exception as ex:
         return size
