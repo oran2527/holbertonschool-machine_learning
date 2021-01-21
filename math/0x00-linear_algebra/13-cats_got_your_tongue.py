04dd80231b9af201a65786033fe2f4b00606b4a1
oran2527 <orlago250183@gmail.com>
2021-01-21 17:21:51 +0000

    fixing line too long line 7

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -4,6 +4,6 @@ import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
-    """ function to return a new numpy.ndarray from two matrices along a specific axis """
+    """ function to return a new array from two matrices along an axis """
 
     return np.concatenate((mat1, mat2), axis)
