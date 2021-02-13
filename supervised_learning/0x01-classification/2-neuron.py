c745adb2232ed4f88e1ba0d734317746dcafac05
oran2527 <orlago250183@gmail.com>
2021-02-13 00:53:40 +0000

    changing message positive integer

--- supervised_learning/0x01-classification/2-neuron.py
+++ supervised_learning/0x01-classification/2-neuron.py
@@ -13,7 +13,7 @@ class Neuron:
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
-            raise ValueError("nx must be an integer")
+            raise ValueError("nx must be a positive integer")
 
         self.__b = 0
         self.__A = 0
