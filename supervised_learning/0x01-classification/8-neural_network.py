0141507b41ee2160964d0737c78e9ebc6ff653b1
oran2527 <orlago250183@gmail.com>
2021-02-14 22:11:48 +0000

    fixing valueerror and reshapes

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -16,12 +16,12 @@ class NeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if type(nodes) is not int:
             raise TypeError("nodes must be an integer")
-        if nx < 1:
+        if nodes < 1:
             raise ValueError("nodes must be a positive integer")
 
-        self.b1 = np.zeros((nodes, 1)).reshape(nodes, 1)
+        self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
-        self.W1 = np.random.randn(nodes, nx).reshape(nodes, nx)
+        self.W1 = np.random.randn(nodes, nx)
 
         self.b2 = 0
         self.A2 = 0
