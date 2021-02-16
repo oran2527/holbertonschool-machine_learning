ddd285e3c8b0ef03a9f4e3512bf69f0e66469ce0
oran2527 <orlago250183@gmail.com>
2021-02-16 01:57:24 +0000

    neural network forward propagation

---
+++ supervised_learning/0x01-classification/10-neural_network.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+""" NeuralNetwork Class """
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """ Class NeuralNetwork """
+
+    def __init__(self, nx, nodes):
+        """ NeuralNetwork initializer """
+
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W1 = np.random.randn(nodes, nx)
+
+        self.__b2 = 0
+        self.__A2 = 0
+        self.__W2 = np.random.randn(nodes).reshape(1, nodes)
+
+    @property
+    def b1(self):
+        """b1 getter"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """A1 getter"""
+        return self.__A1
+
+    @property
+    def W1(self):
+        """W1 getter"""
+        return self.__W1
+
+    @property
+    def b2(self):
+        """b2 getter"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """A2 getter"""
+        return self.__A2
+
+    @property
+    def W2(self):
+        """W2 getter"""
+        return self.__W2
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neural network"""
+
+        XT = np.transpose(X)
+        W1T = np.transpose(self.W1)
+        A_1 = np.transpose(np.matmul(XT, W1T)) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-1 * A_1))
+
+        A1T = np.transpose(self.A1)
+        W2T = np.transpose(self.W2)
+        A_2 = np.transpose(np.matmul(A1T, W2T)) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-1 * A_2))
+
+        return self.__A1, self.__A2
