80af057d5048073c14287ba01b0ee398f03c6074
oran2527 <orlago250183@gmail.com>
2021-02-18 20:13:46 +0000

    accuracy

---
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Accuracy with TensorFlow """
+import tensorflow as tf
+
+
+def calculate_accuracy(y, y_pred):
+    """ 
+    y is a placeholder for the labels of the input data
+    y_pred is a tensor containing the network’s predictions
+    Returns: a tensor containing the decimal accuracy of the prediction
+
+    """
+    c_p = tf.equal(tf.argmax(y, 1), tf.argmax(y_pred, 1))
+    a = tf.reduce_mean(tf.cast(c_p, tf.float32))
+    return a
