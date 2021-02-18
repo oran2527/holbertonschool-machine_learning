bff5609552f00ebd1b3f32bacf571ca9914404ec
oran2527 <orlago250183@gmail.com>
2021-02-18 20:15:42 +0000

    pycodestyle

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 
 
 def calculate_accuracy(y, y_pred):
-    """ 
+    """
     y is a placeholder for the labels of the input data
     y_pred is a tensor containing the network’s predictions
     Returns: a tensor containing the decimal accuracy of the prediction
