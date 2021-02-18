1ee7b16a8f5aadbfe3dbcdb3dd05992eaf811a1a
oran2527 <orlago250183@gmail.com>
2021-02-18 20:05:41 +0000

    creating forward prop

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python3
-"""forward propagation"""
+""" Forward propagation using tensorflow """
+import tensorflow as tf
 
 create_layer = __import__('1-create_layer').create_layer
 
 
 def forward_prop(x, layer_sizes=[], activations=[]):
     """
-    creates the forward propagation graph for the neural network:
-
     x is the placeholder for the input data
-    layer_sizes is a list containing the number of nodes in each layer of
-    the network
-    activations is a list containing the activation functions for each
+    layer_sizes is a list containing the number of nodes in each
     layer of the network
+    activations is a list containing the activation functions for
+    each layer of the network
     Returns: the prediction of the network in tensor form
-    For this function, you should import your create_layer function with
-    create_layer = __import__('1-create_layer').create_layer
 
     """
-
-    la = create_layer(x, layer_sizes[0], activations[0])
-
-    for i in range(1, len(layer_sizes)):
-        la = create_layer(la, layer_sizes[i], activations[i])
-    return layer
+    if len(layer_sizes) and len(activations):
+        if len(layer_sizes) == len(activations):
+            A = x
+            for i in range(len(layer_sizes)):
+                A = create_layer(A, layer_sizes[i], activations[i])
+            return A
+    return None
