f6683b19acf4badee54e833a53847ed6250b8b25
oran2527 <orlago250183@gmail.com>
2021-03-09 14:25:31 +0000

    fix error

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -4,7 +4,8 @@ import tensorflow.keras as K
 
 
 def optimize_model(network, alpha, beta1, beta2):
-    """ Optimizer using keras """    opt = K.optimizers.Adam(alpha, beta1, beta2)
+    """ Optimizer using keras """
+    opt = K.optimizers.Adam(alpha, beta1, beta2)
     network.compile(optimizer=opt, loss='categorical_crossentropy',
                     metrics=['accuracy'])
     return None
