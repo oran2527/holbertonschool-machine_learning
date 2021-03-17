9e648c41fba1692226d6519650d00e94cd2613b6
oran2527 <orlago250183@gmail.com>
2021-03-17 16:31:41 +0000

    fix error

--- supervised_learning/0x03-optimization/6-main.py
+++ supervised_learning/0x03-optimization/6-main.py
@@ -12,7 +12,7 @@ def one_hot(Y, classes):
     return one_hot
 
 if __name__ == '__main__':
-    lib= np.load('../data/MNIST.npz')
+    lib= np.load('/vagrant/data/MNIST.npz')
     X_3D = lib['X_train']
     Y = lib['Y_train']
     X = X_3D.reshape((X_3D.shape[0], -1))
