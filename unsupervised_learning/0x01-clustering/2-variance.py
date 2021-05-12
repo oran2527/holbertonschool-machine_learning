535266599415a27f33fc4663d8d470b1a36956c8
oran2527 <orlago250183@gmail.com>
2021-05-12 18:19:58 +0000

    total intra-cluster variance for a data set

---
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Variance """
+import numpy as np
+
+
+def variance(X, C):
+    """ Variance """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None
+    if type(C) is not np.ndarray or len(C.shape) != 2:
+        return None
+    if X.shape[0] < C.shape[0] or X.shape[1] != C.shape[1]:
+        return None
+    distances = np.linalg.norm(X - C[:, np.newaxis], axis=2)
+    clss = np.min(distances, axis=0)
+    var = (clss**2).sum()
+    return var.sum()
