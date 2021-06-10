2e2e4381b2ca27b7d0f47e994b770390f383ed71
oran2527 <orlago250183@gmail.com>
2021-06-10 14:44:57 +0000

    hidden state in the backward direction for one time step

---
+++ supervised_learning/0x0D-RNNs/6-bi_backward.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Bidirectional Cell Forward """
+import numpy as np
+
+
+class BidirectionalCell:
+    """ Bidirectional Cell Forward """
+    def __init__(self, i, h, o):
+        """ Bidirectional Cell Forward """
+        self.Whf = np.random.normal(0, 1, (i + h, h))
+        self.Whb = np.random.normal(0, 1, (i + h, h))
+        self.Wy = np.random.normal(0, 1, (h * 2, o))
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """ Bidirectional Cell Forward """
+        h = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(np.dot(h, self.Whf) + self.bhf)
+        return h_next
+
+    def backward(self, h_next, x_t):
+        """ Bidirectional Cell Forward """
+        h = np.concatenate((h_next, x_t), axis=1)
+        h_prev = np.tanh(np.dot(h, self.Whb) + self.bhb)
+        return h_prev
