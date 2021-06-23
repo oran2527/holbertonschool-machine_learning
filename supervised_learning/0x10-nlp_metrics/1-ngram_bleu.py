6e45415c4b2cc177d08f7f81679d881f1ac94889
oran2527 <orlago250183@gmail.com>
2021-06-23 13:47:47 +0000

    unigram BLEU score for a sentence

---
+++ supervised_learning/0x10-nlp_metrics/1-ngram_bleu.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+""" Ngram BLEU score """
+import numpy as np
+
+
+def create_ngram(sentence, n):
+    """ Ngram BLEU score """
+    new_sentence = []
+    for i in range(len(sentence) - n + 1):
+        n_gram = ""
+        for j in range(n):
+            n_gram += sentence[i + j]
+            if not j + 1 == n:
+                n_gram += " "
+        new_sentence.append(n_gram)
+    return new_sentence
+
+
+def ngram_bleu(references, sentence, n):
+    """ Ngram BLEU score """
+    c = len(sentence)
+    # Calculate r, closest length reference to candidate
+    r_list = np.array([abs(len(ref) - c) for ref in references])
+    # Take indices whit the same distance
+    mask = np.where(r_list == r_list.min())
+    # Apply mask and take the minimum length in masked references
+    r = np.array([len(ref) for ref in references])[mask].sum()
+
+    cn = create_ngram(sentence, n)
+    candidate = {x: 0 for x in cn}
+    references = [create_ngram(ref, n) for ref in references]
+
+    max_match = 0
+    for ref in references:
+        match = 0
+        ref_dict = {x: ref.count(x) for x in ref}
+        for key in ref_dict.keys():
+            if key in candidate:
+                match += 1
+        if match > max_match:
+            max_match = match
+    P = max_match / len(cn)
+    if c > r:
+        BP = 1
+    else:
+        BP = np.exp(1-(r / c))
+    return BP * P
