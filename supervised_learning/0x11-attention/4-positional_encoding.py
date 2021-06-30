4a83c91003b5db7841fc1b00132e95f4d4245512
oran2527 <orlago250183@gmail.com>
2021-06-30 15:07:28 +0000

    calculates the positional encoding for a transformer

---
+++ supervised_learning/0x11-attention/4-positional_encoding.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Positional Encoding """
+import numpy as np
+
+
+def get_angles(pos, i, d_model):
+    """ Positional Encoding """
+    angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
+    return pos * angle_rates
+
+
+def positional_encoding(max_seq_len, dm):
+    """ Positional Encoding """
+    angle_rads = get_angles(np.arange(max_seq_len)[:, np.newaxis],
+                            np.arange(dm)[np.newaxis, :],
+                            dm)
+    # apply sin to even indices in the array; 2i
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+    # apply cos to odd indices in the array; 2i+1
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+
+    return angle_rads
