77f30a5e11e05714a5f6abc2dc1f51dbd9b67a07
oran2527 <orlago250183@gmail.com>
2021-09-04 20:48:53 +0000

    list of ships that can hold a given number of passengers

---
+++ pipeline/0x01-apis/0-passengers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Passengers """
+import requests as rq
+
+
+def availableShips(passengerCount):
+    """ Passengers """
+    vehicles = []
+    page = 1
+    state = True
+
+    while state:
+        url = "https://swapi-api.hbtn.io/api/starships/?page=" + str(page)
+        r = rq.get(url)
+        data = r.json()
+        results = data['results']
+        for vehicle in results:
+            passenger = vehicle['passengers']
+            passenger = passenger.replace(',', "")
+            if passenger.isnumeric() and int(passenger) >= passengerCount:
+                vehicles.append(vehicle['name'])
+        if data['next'] is None:
+            state = False
+        page += 1
+
+    return (vehicles)
