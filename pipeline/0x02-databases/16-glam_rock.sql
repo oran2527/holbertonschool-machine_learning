3ec49e8d31c5fe4025ba9f6af1440ea426b99038
oran2527 <orlago250183@gmail.com>
2021-09-09 13:28:01 +0000

    lists all bands with Glam rock as their main style, ranked by their longevity

---
+++ pipeline/0x02-databases/16-glam_rock.sql
@@ -0,0 +1,10 @@
+-- lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name,
+        IF(split IS NULL,
+           (2020-formed),
+           (split - formed))
+           AS lifespan
+           FROM metal_bands
+           WHERE style
+           like '%Glam Rock%'
+           ORDER BY lifespan DESC;
