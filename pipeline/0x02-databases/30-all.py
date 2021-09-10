20dfbd33e65e172a8ae629461deb0302f244aaae
oran2527 <orlago250183@gmail.com>
2021-09-10 13:16:32 +0000

    Python function that lists all documents in a collection

---
+++ pipeline/0x02-databases/30-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Python function that lists all documents in a collection """
+
+def list_all(mongo_collection):
+    """ Python function that lists all documents in a collection """
+    docs = []
+    collection = mongo_collection.find()
+    for doc in collection:
+        docs.append(doc)
+    return docs
