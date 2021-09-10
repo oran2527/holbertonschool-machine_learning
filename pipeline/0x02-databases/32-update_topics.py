8335616fb9eb8ef16ec9c724ea540a0d7580a1cd
oran2527 <orlago250183@gmail.com>
2021-09-10 23:30:11 +0000

    changes all topics of a school document based on the name

---
+++ pipeline/0x02-databases/32-update_topics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" changes all topics of a school document based on the name """
+
+
+def update_topics(mongo_collection, name, topics):
+    """ changes all topics of a school document based on the name """
+    newvalues = {"$set": {"topics": topics}}
+    mongo_collection.update_many({"name": name}, newvalues)
